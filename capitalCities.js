f468304749051312700f43d845c8e9f9ec27442b
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2023-11-12 10:21:01 +0200

    Add files via upload

--- capitalCities.js
+++ capitalCities.js
@@ -1,3 +1,3 @@
 function capitalCities  (){
-alert("Thanks for using this App, see you again.")
+alert("Thanks for using this App, a'm actually working on Some other interesting projects,  So hope to see you again Salmanda is my name.")
 }
