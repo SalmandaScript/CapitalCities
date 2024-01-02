f797720bbca057f6abb2e60ddca1f905f14ea5f7
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-01-02 12:44:53 +0200

    Add files via upload

--- capitalCities.js
+++ capitalCities.js
@@ -1,3 +1,7 @@
 function capitalCities  (){
 alert("Thanks for using this App, a'm actually working on Some other interesting projects,  So hope to see you again Salmanda is my name.")
 }
+
+function africaOnline(){
+open("https://salmandascript.github.io/AfricaOnline/");
+}
